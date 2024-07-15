c9874011b3da4a6b8f44acdfae09baf39ae77fec
DujalSawarkar <dujalsawar5@gmail.com>
2024-07-15 20:35:42 +0530

    feat : navbar updated

--- src/app/(components)/Navbar.tsx
+++ src/app/(components)/Navbar.tsx
@@ -3,28 +3,77 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { FaShoppingCart } from "react-icons/fa";
-import { RiSearchLine, RiArrowDropDownLine } from "react-icons/ri";
+import { FaBars, FaShoppingCart } from "react-icons/fa";
+import { RiSearchLine } from "react-icons/ri";
 import shopco from "@/app/imgs/SHOP.CO.png";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 const Navbar = () => {
   return (
     <div className="bg-white shadow-sm">
-      <div className=" mx-auto flex items-center p-2 justify-between h-[11vh]">
-        <div className="flex-shrink-0 pl-5">
-          <Link href="/">
-            <Image src={shopco} alt="Your Logo" width={208} height={52} />
-          </Link>
+      <div className="mx-auto flex items-center p-2 justify-between h-[11vh]">
+        <div className="flex items-center space-x-6">
+          <Sheet>
+            <SheetTrigger asChild>
+              <button className="lg:hidden text-gray-900 text-2xl hover:text-gray-600">
+                <FaBars />
+              </button>
+            </SheetTrigger>
+            <SheetContent side="left">
+              <SheetHeader>
+                <SheetTitle>Menu</SheetTitle>
+                {/* <SheetDescription>Select a category</SheetDescription> */}
+              </SheetHeader>
+              <div className="flex flex-col space-y-2">
+                <Link
+                  href="/category"
+                  className="flex items-center text-gray-900 text-lg font-medium hover:text-gray-600"
+                >
+                  Shop
+                </Link>
+                <Link
+                  href="/category"
+                  className="text-gray-900 text-lg font-medium hover:text-gray-600"
+                >
+                  On Sales
+                </Link>
+                <Link
+                  href="/category"
+                  className="text-gray-900 text-lg font-medium hover:text-gray-600"
+                >
+                  New Arrivals
+                </Link>
+                <Link
+                  href="/category"
+                  className="text-gray-900 text-lg font-medium hover:text-gray-600"
+                >
+                  Brands
+                </Link>
+              </div>
+            </SheetContent>
+          </Sheet>
+          <div className="flex-shrink-0 pl-5">
+            <Link href="/">
+              <Image src={shopco} alt="Your Logo" width={208} height={52} />
+            </Link>
+          </div>
         </div>
-        <div className="flex-1 flex justify-center items-center mx-6">
+        <div className="hidden lg:flex flex-1 justify-center items-center mx-6">
           <div className="flex space-x-6">
             <Link
               href="/category"
               className="flex items-center text-gray-900 text-lg font-medium hover:text-gray-600"
             >
-              Shop <RiArrowDropDownLine className="ml-1" />
+              Shop
             </Link>
             <Link
               href="/category"
@@ -61,8 +110,6 @@ const Navbar = () => {
           >
             <FaShoppingCart />
           </Link>
-
-          {/* <HiOutlineUserCircle /> */}
           <SignedOut>
             <SignInButton afterSignUpUrl="/" afterSignInUrl="/" mode="redirect">
               <Button>Sign In</Button>
